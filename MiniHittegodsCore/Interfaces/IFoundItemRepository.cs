4aa0d86db0dc1dc468c064cdd7bafdec2a05b023
runarer <95697611+runarer@users.noreply.github.com>
2026-03-04 13:49:33 +0100

    Repository interface

---
+++ MiniHittegodsCore/Interfaces/IFoundItemRepository.cs
@@ -0,0 +1,16 @@
+
+using MiniHittegodsCore.Model;
+
+namespace MiniHittegodsCore.Interfaces;
+
+public interface IFoundItemRepository
+{
+    Task AddFoundItemAsync(FoundItem foundItem);
+
+    Task DeleteFoundItemAsync(Guid id);
+
+    Task<FoundItem> GetFoundItemAsync(Guid id);
+    Task<List<FoundItem>> GetAllFoundItemsAsync();
+
+    Task UpdateFoundItem(FoundItem foundItem);
+}
