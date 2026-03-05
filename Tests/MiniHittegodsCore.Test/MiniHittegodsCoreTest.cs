8e4bc6b29fe567fb7ebbc79e67440bbceb956b72
runarer <95697611+runarer@users.noreply.github.com>
2026-03-05 14:35:09 +0100

    removed unused usings

--- Tests/MiniHittegodsCore.Test/MiniHittegodsCoreTest.cs
+++ Tests/MiniHittegodsCore.Test/MiniHittegodsCoreTest.cs
@@ -1,7 +1,4 @@
 
-
-using System.ComponentModel.DataAnnotations;
-using System.Security.Claims;
 using Microsoft.Extensions.Time.Testing;
 using MiniHittegodsCore.Interfaces;
 using MiniHittegodsCore.Model;
