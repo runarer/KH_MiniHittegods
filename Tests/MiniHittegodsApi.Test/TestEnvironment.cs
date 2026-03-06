23f089845750ac0f3255abe7667b7ca7d5fc595e
runarer <95697611+runarer@users.noreply.github.com>
2026-03-06 10:19:06 +0100

    [Red] Added some helper functions for the testing

--- Tests/MiniHittegodsApi.Test/TestEnvironment.cs
+++ Tests/MiniHittegodsApi.Test/TestEnvironment.cs
@@ -1,5 +1,8 @@
 using System.Net.Http.Json;
 using Microsoft.AspNetCore.Mvc.Testing;
+using MiniHittegodsApi.DTOs;
+using MiniHittegodsCore.Model;
+using MiniHittegodsCore.Model.DTO;
 
 namespace MiniHittegodsApi.Test;
 
@@ -19,4 +22,34 @@ public class TestEnvironment : IClassFixture<WebApplicationFactory<Program>>
         await _factory.DisposeAsync();
         _factory = new WebApplicationFactory<Program>();
     }
+
+    protected static async Task<HttpResponseMessage> CreateAnItemOnTheServer(HttpClient client, FoundItemPostRequestDTO foundItem)
+    {
+        var response = await client.PostAsJsonAsync("api/items", foundItem);
+        response.EnsureSuccessStatusCode();
+
+        return response;
+    }
+
+    protected static async Task<Uri> GetLocationOfResponse(HttpResponseMessage response)
+    {
+        Assert.NotNull(response.Headers.Location);
+        return response.Headers.Location;
+    }
+
+    protected static async Task<FoundItemResponseDTO> GetFoundItemResponse(HttpResponseMessage response)
+    {
+        var createdNoteResponse = await response.Content.ReadFromJsonAsync<FoundItemResponseDTO>();
+        Assert.NotNull(createdNoteResponse);
+        return createdNoteResponse;
+    }
+
+    protected readonly FoundItemPostRequestDTO[] foundItems =
+    [
+        new FoundItemPostRequestDTO("Test item with index 0","Test description for item 0",Category.Other,"Test found location for item 0"),
+        new FoundItemPostRequestDTO("Test item with index 1","Test description for item 1",Category.Keys,"Test found location for item 1"),
+        new FoundItemPostRequestDTO("Test item with index 2","Test description for item 2",Category.Clothning,"Test found location for item 2"),
+        new FoundItemPostRequestDTO("Test item with index 3","Test description for item 3",Category.Wallet,"Test found location for item 3"),
+        new FoundItemPostRequestDTO("Test item with index 4","Test description for item 4",Category.Other,"Test found location for item 4"),
+    ];
 }
