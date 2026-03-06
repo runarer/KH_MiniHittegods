8bd6a4c3237baf42066914cb9a0c3be85c33a019
runarer <95697611+runarer@users.noreply.github.com>
2026-03-06 09:57:48 +0100

    [Red] Test for creating a FoundItem

--- Tests/MiniHittegodsApi.Test/RegisterFoundItemTest.cs
+++ Tests/MiniHittegodsApi.Test/RegisterFoundItemTest.cs
@@ -1,17 +1,41 @@
+using System.Net;
+using System.Net.Http.Json;
+using Microsoft.AspNetCore.Mvc.Testing;
+using MiniHittegodsApi.DTOs;
+using MiniHittegodsCore.Model;
+
 namespace MiniHittegodsApi.Test;
 
-public class RegisterFoundItemTest
+public class RegisterFoundItemTest(WebApplicationFactory<Program> factory) : TestEnvironment(factory)
 {
-    [Fact(Skip = "Test not implemented")]
-    public void RegisterItem_RegisterAValidItem_Returns201LocationAndObjectCreated()
+    [Fact]
+    public async Task RegisterItem_RegisterAValidItem_Returns201LocationAndObjectCreated()
     {
+        var client = Client;
+        var foundItemPostRequestDTO = new FoundItemPostRequestDTO("Test Item", "Test item description", Category.Other, "Alley behind the abandoned mansion.");
 
+        var response = await client.PostAsJsonAsync("api/items", foundItemPostRequestDTO);
+        response.EnsureSuccessStatusCode();
+
+        var createResponse = await response.Content.ReadFromJsonAsync<FoundItemResponseDTO>();
+        Assert.NotNull(createResponse);
+        Assert.NotEqual(Guid.Empty, createResponse.Id);
+        Assert.Equal(foundItemPostRequestDTO.Title, createResponse.Title);
+        Assert.Equal(foundItemPostRequestDTO.Description, createResponse.Description);
+        Assert.Equal(foundItemPostRequestDTO.Category, createResponse.Category);
+        Assert.Equal(foundItemPostRequestDTO.FoundLocation, createResponse.FoundLocation);
+        Assert.Equal(Status.Available, createResponse.Status);
+        Assert.NotEqual(default, createResponse.FoundAtUtc);
+        Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+        Assert.NotNull(response.Headers.Location);
+        Assert.Contains($"/api/items/{createResponse.Id}", response.Headers.Location.ToString());
     }
     [Fact(Skip = "Test not implemented")]
     public void RegisterItem_RegisterAnItemWithoutTitle_Returns400AndErrorMessage()
     {
 
     }
+
     [Fact(Skip = "Test not implemented")]
     public void RegisterItem_RegisterAnItemWithoutFoundLocation_Returns400AndErrorMessage()
     {
