87d812e60609d4ee38b86fe5e06292deeb2691b0
runarer <95697611+runarer@users.noreply.github.com>
2026-03-06 09:20:42 +0100

    Adds test environment and test package

---
+++ Tests/MiniHittegodsApi.Test/TestEnvironment.cs
@@ -0,0 +1,22 @@
+using System.Net.Http.Json;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace NoteStorageTests;
+
+public class TestEnvironment : IClassFixture<WebApplicationFactory<Program>>
+{
+    protected WebApplicationFactory<Program> _factory;
+
+    public TestEnvironment(WebApplicationFactory<Program> factory)
+    {
+        _factory = factory;
+    }
+
+    public HttpClient Client => _factory.CreateClient();
+
+    public async Task RestartAsync()
+    {
+        await _factory.DisposeAsync();
+        _factory = new WebApplicationFactory<Program>();
+    }
+}
