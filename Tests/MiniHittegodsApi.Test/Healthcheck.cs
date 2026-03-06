207ff45eee73e69e6b4f8228a4fa86b7269739e2
runarer <95697611+runarer@users.noreply.github.com>
2026-03-06 09:57:13 +0100

    [Red] Do not pass healthcheck
    Server is not yet implemented so this is expected.

---
+++ Tests/MiniHittegodsApi.Test/Healthcheck.cs
@@ -0,0 +1,17 @@
+
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace MiniHittegodsApi.Test;
+
+public class HealthCheck(WebApplicationFactory<Program> factory) : TestEnvironment(factory)
+{
+    [Fact]
+    public async Task Get_EndpointReturnSuccess()
+    {
+        var client = _factory.CreateClient();
+
+        var response = await client.GetAsync("/health");
+
+        response.EnsureSuccessStatusCode();
+    }
+}
