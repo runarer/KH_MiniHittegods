1c4b0d8ab50eac1585b768c8a903c30b9609ce62
runarer <95697611+runarer@users.noreply.github.com>
2026-03-06 09:42:33 +0100

    TestEnvirorment for all the test classes

--- Tests/MiniHittegodsApi.Test/TestEnvironment.cs
+++ Tests/MiniHittegodsApi.Test/TestEnvironment.cs
@@ -1,7 +1,7 @@
 using System.Net.Http.Json;
 using Microsoft.AspNetCore.Mvc.Testing;
 
-namespace NoteStorageTests;
+namespace MiniHittegodsApi.Test;
 
 public class TestEnvironment : IClassFixture<WebApplicationFactory<Program>>
 {
