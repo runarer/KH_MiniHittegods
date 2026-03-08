de7b7fad13f9e956e0bd29a12832bf1580c3afe7
runarer <95697611+runarer@users.noreply.github.com>
2026-03-08 18:28:51 +0100

    Adds test for delete

--- Tests/MiniHittegodsApi.Test/DeleteItemTest.cs
+++ Tests/MiniHittegodsApi.Test/DeleteItemTest.cs
@@ -1,24 +1,56 @@
-using System;
+
+using System.Net;
+using System.Net.Http.Json;
+using Microsoft.AspNetCore.Mvc.Testing;
+using MiniHittegodsApi.DTOs;
 
 namespace MiniHittegodsApi.Test;
 
-public class DeleteFoundTest
+public class DeleteFoundTest(WebApplicationFactory<Program> factory) : TestEnvironment(factory)
 {
-    [Fact(Skip = "Test not implemented")]
-    public void DeleteItem_DeleteItemWithIdItemExcistsAndGotStatusAsAvailable_Return204()
+    [Fact]
+    public async Task DeleteItem_DeleteItemWithIdItemExcistsAndGotStatusAsAvailable_Return204()
     {
+        var client = Client;
+        var createdFoundItem = await CreateAnItemOnTheServer(client, foundItems[0]);
+        var createdFoundItemLocation = await GetLocationOfResponse(createdFoundItem);
 
+        var response = await client.DeleteAsync(createdFoundItemLocation);
+
+        response.EnsureSuccessStatusCode();
+
+        var responseGetDeletedFoundItem = await client.GetAsync(createdFoundItemLocation);
+        Assert.Equal(HttpStatusCode.NotFound, responseGetDeletedFoundItem.StatusCode);
     }
 
-    [Fact(Skip = "Test not implemented")]
-    public void DeleteItem_DeleteItemWithIdItemDoesntExcist_Return404AndErrorMessage()
+    [Fact]
+    public async Task DeleteItem_DeleteItemWithIdItemDoesntExcist_Return404AndErrorMessage()
     {
+        var client = Client;
+        var nonExistingFoundItemLocation = $"/api/items/{Guid.NewGuid()}";
 
+        var response = await client.DeleteAsync(nonExistingFoundItemLocation);
+
+        Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        var notFoundRequest = await response.Content.ReadAsStringAsync();
+        Assert.Contains("Item not found!", notFoundRequest);
     }
 
-    [Fact(Skip = "Test not implemented")]
-    public void DeleteItem_DeleteItemWithIdItemExcistsButStatusIsNotSetToAvailable_Return409()
+    [Fact]
+    public async Task DeleteItem_DeleteItemWithIdItemExcistsButStatusIsNotSetToAvailable_Return409()
     {
+        var client = Client;
+        var createdFoundItem = await CreateAnItemOnTheServer(client, foundItems[0]);
+        var createdFoundItemLocation = await GetLocationOfResponse(createdFoundItem);
+        var claimeItemResponse = await client.PostAsJsonAsync(
+            createdFoundItemLocation + "/claim",
+            new FoundItemClaimRequestDTO("Test claimer"));
+        claimeItemResponse.EnsureSuccessStatusCode();
 
+        var response = await client.DeleteAsync(createdFoundItemLocation);
+
+        Assert.Equal(HttpStatusCode.Conflict, response.StatusCode);
+        var notFoundRequest = await response.Content.ReadAsStringAsync();
+        Assert.Contains("Item cannot be deleted!", notFoundRequest);
     }
 }
