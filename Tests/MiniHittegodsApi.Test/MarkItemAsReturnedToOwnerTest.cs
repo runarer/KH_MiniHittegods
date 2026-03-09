5730e6915621409e759036d2e69461bb9aa0b749
runarer <95697611+runarer@users.noreply.github.com>
2026-03-09 12:34:40 +0100

    [Red] Tests for return item

--- Tests/MiniHittegodsApi.Test/MarkItemAsReturnedToOwnerTest.cs
+++ Tests/MiniHittegodsApi.Test/MarkItemAsReturnedToOwnerTest.cs
@@ -1,24 +1,59 @@
 
 
+using System.Net;
+using System.Net.Http.Json;
+using MiniHittegodsApi.DTOs;
+using MiniHittegodsCore.Model;
+
 namespace MiniHittegodsApi.Test;
 
-public class MarkItemAsReturnedToOwnerTest
+public class MarkItemAsReturnedToOwnerTest(CustomWebApplicationFactory<Program> factory) : TestEnvironment(factory)
 {
-    [Fact(Skip = "Test not implemented")]
-    public void ReturnItem_MarkItemAsReturnedItemExcistsAndGotStatusAsClaimed_Return200WithItemAndSetStatusToReturnedAndTimeItWasClaimed()
+    [Fact]
+    public async Task ReturnItem_MarkItemAsReturnedItemExcistsAndGotStatusAsClaimed_Return200WithItemAndSetStatusToReturnedAndTimeItWasClaimed()
     {
+        var client = Client;
+        var foundItemResponse = await CreateAnItemOnTheServer(client, foundItems[0]);
+        foundItemResponse.EnsureSuccessStatusCode();
+        var location = await GetLocationOfResponse(foundItemResponse);
+        var claimer = "Test claimer";
+        var claimedItemResponse = await client.PostAsJsonAsync(location + "/claim", new FoundItemClaimRequestDTO(claimer));
+        claimedItemResponse.EnsureSuccessStatusCode();
+
+        var returnedFoundItemResponse = await client.PostAsync(location + "/return", null);
+
+        returnedFoundItemResponse.EnsureSuccessStatusCode();
+        var returnedFoundItem = await GetFoundItemResponse(returnedFoundItemResponse);
+        Assert.Equal(Status.Returned, returnedFoundItem.Status);
+        Assert.NotEqual(default, returnedFoundItem.ReturnedAtUtc);
 
     }
 
-    [Fact(Skip = "Test not implemented")]
-    public void ReturnItem_MarkItemAsReturnedItemDoedntExcists_Return404AndErrorMessage()
+    [Fact]
+    public async Task ReturnItem_MarkItemAsReturnedItemDoedntExcists_Return404AndErrorMessage()
     {
+        var client = Client;
+
+        var response = await client.PostAsJsonAsync($"/api/items/{Guid.NewGuid()}/return", new FoundItemClaimRequestDTO("Test claimer"));
+
+        Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        var notFoundRequest = await response.Content.ReadAsStringAsync();
+        Assert.Contains("Item not found!", notFoundRequest);
 
     }
 
-    [Fact(Skip = "Test not implemented")]
-    public void ReturnItem_MarkItemAsReturnedItemExcistsButDoNotGotStatusAsClaimed_Return409AndErrorMessage()
+    [Fact]
+    public async Task ReturnItem_MarkItemAsReturnedItemExcistsButDoNotGotStatusAsClaimed_Return409AndErrorMessage()
     {
+        var client = Client;
+        var foundItemResponse = await CreateAnItemOnTheServer(client, foundItems[0]);
+        foundItemResponse.EnsureSuccessStatusCode();
+        var location = await GetLocationOfResponse(foundItemResponse);
 
+        var claimClaimedItemResponse = await client.PostAsync(location + "/return", null);
+
+        Assert.Equal(HttpStatusCode.Conflict, claimClaimedItemResponse.StatusCode);
+        var notFoundRequest = await claimClaimedItemResponse.Content.ReadAsStringAsync();
+        Assert.Contains("Item not claimed!", notFoundRequest);
     }
 }
